d19828e060c6b835351ad9af830c8eedf205eb16
TanHui <915156161@qq.com>
2021-08-17 11:20:41 +0800

    improve performance

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
  * @Author: TanHui
  * @Date: 2021-08-17 11:19:29
  * @LastEditors: TanHui
- * @LastEditTime: 2021-08-17 11:19:41
+ * @LastEditTime: 2021-08-17 11:20:26
  * @Description:
  */
 console.log("its my life")
+console.log("improve performance")
